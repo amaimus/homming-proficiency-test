be075f655859a2e44400d55330b45b2fb0987625
Alejandro <amaimus@boardfy.com>
2022-02-27 14:13:18 +0100

    tailwind configured properly, dependencies added as prod dependencies

--- resources/App.vue
+++ resources/App.vue
@@ -1,11 +1,15 @@
 <template>
   <div id="app">
     WORK HERE
+    <h1 class="text-3xl font-bold underline">
+      Hello world! Tailwind test
+    </h1>
   </div>
 </template>
 
 <script>
 import { users, propertyTypes, properties } from '@/mocks/api';
+import './main.css';
 
 export default {
   name: 'App',
